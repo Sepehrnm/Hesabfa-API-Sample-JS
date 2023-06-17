d35a1dd016774dab557ab468308f671852e4df25
Sepehr <sepehrn249@gmail.com>
2023-06-17 12:32:45 +0330

    some pages UI with methods initialized and codes refactored

--- API.js
+++ API.js
@@ -26,37 +26,36 @@ async function getContact(data) {
     let method = 'contact/get';
     let result = await apiRequest(method, data);
     return (result.Result);
-    showData(result.Result);
 }
 
 async function getContactById(data) {
     let method = 'contact/getById';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getContacts(data) {
     let method = 'contact/getcontacts';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function saveContact(data) {
     let method = 'contact/save';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function deleteContact(data) {
     let method = 'contact/delete';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getContactLink(data) {
     let method = 'contact/getContactLink';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 //Items Methods
@@ -64,49 +63,50 @@ async function getContactLink(data) {
 async function getItem(data) {
     let method = 'item/get';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getItemByBarcode(data) {
     let method = 'item/getByBarcode';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getItemById(data) {
     let method = 'item/getById';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function getitems(data) {
+async function getItems(data) {
     let method = 'item/getitems';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result.List);
 }
 
 async function saveItem(data) {
     let method = 'item/save';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function deleteItem(data) {
     let method = 'item/delete';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function GetItemQuantity(data) {
+async function getItemQuantity() {
     let method = 'item/GetQuantity';
+    let data = '';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function UpdateItemOpeningQuantity(data) {
     let method = 'item/UpdateOpeningQuantity';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 // Invoices Methods
@@ -114,49 +114,49 @@ async function UpdateItemOpeningQuantity(data) {
 async function getInvoice(data) {
     let method = 'invoice/get';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getInvoiceById(data) {
     let method = 'invoice/getById';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getInvoices(data) {
     let method = 'invoice/getinvoices';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function saveInvoice(data) {
     let method = 'invoice/save';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function deleteInvoice(data) {
     let method = 'invoice/delete';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function savePayment(data) {
     let method = 'invoice/savepayment';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getOnlineInvoiceURL(data) {
     let method = 'invoice/getonlineinvoiceurl';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function SaveWarehouseReceipt(data) {
     let method = 'invoice/SaveWarehouseReceipt';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 // ReceivePay Methods
@@ -164,25 +164,25 @@ async function SaveWarehouseReceipt(data) {
 async function getReceipt(data) {
     let method = 'receipt/get';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getReceipts(data) {
     let method = 'receipt/getReceipts';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function saveReceipt(data) {
     let method = 'receipt/save';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function deleteReceipt(data) {
     let method = 'receipt/delete';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 // Accounting Methods
@@ -190,129 +190,129 @@ async function deleteReceipt(data) {
 async function getDocument(data) {
     let method = 'document/get';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function saveDocument(data) {
     let method = 'document/save';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function getDocuments(data) {
     let method = 'document/getdocuments';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function deleteDocument(data) {
     let method = 'document/delete';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 // Other Methods
 
-async function deleteDocument(data) {
+async function getChanges(data) {
     let method = 'setting/GetChanges';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getFiscalYears(data) {
     let method = 'setting/GetFiscalYears';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getBanks(data) {
     let method = 'setting/GetBanks';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getCashes(data) {
     let method = 'setting/GetCashes';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getPettyCashes(data) {
     let method = 'setting/GetPettyCashes';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getCurrency(data) {
     let method = 'setting/GetCurrency';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getWarehouses(data) {
     let method = 'setting/GetWarehouses';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getProductCategories(data) {
     let method = 'setting/GetProductCategories';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getServiceCategories(data) {
     let method = 'setting/GetServiceCategories';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getContactCategories(data) {
     let method = 'setting/GetContactCategories';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getFiscalYear(data) {
     let method = 'setting/GetFiscalYear';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getProjects(data) {
     let method = 'setting/GetProjects';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getSalesmen(data) {
     let method = 'setting/GetSalesmen';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getCurrencyTable(data) {
     let method = 'setting/GetCurrencyTable';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function setCurrencyTable(data) {
     let method = 'setting/SetCurrencyTable';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getAccounts(data) {
     let method = 'setting/GetAccounts';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
-async function deleteDocument(data) {
+async function getBusinessInfo(data) {
     let method = 'setting/GetBusinessInfo';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 // Reports Methods
@@ -320,55 +320,35 @@ async function deleteDocument(data) {
 async function balancesheet(data) {
     let method = 'report/balancesheet';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function reportDebtorscreditors(data) {
     let method = 'report/debtorscreditors';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function reportInventory(data) {
     let method = 'report/inventory';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function profitAndLoss(data) {
     let method = 'report/profitandlossstatement';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function trialBalance(data) {
     let method = 'report/trialbalance';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
 
 async function trialBalanceItems(data) {
     let method = 'report/trialbalanceitems';
     let result = await apiRequest(method, data);
-    showData(result.Result);
+    return (result.Result);
 }
-
-// Show Data
-
-// function showData(output) {
-//     let table = document.getElementById('table');
-//     for (const [key, value] of Object.entries(output)) {
-//         var tableItem = `
-//             <tr>
-//                 <th>
-//                     ${key}
-//                 </th>
-//                 <td>
-//                     ${value}
-//                 </td>
-//             </tr>
-//         `;
-//
-//         table.innerHTML += (tableItem);
-//     }
-// }
